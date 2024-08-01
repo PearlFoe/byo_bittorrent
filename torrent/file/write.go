bb7361ea8afcedf1f2f6d401d09f11f7cd23eb81
PearlFoe <itknml@gmail.com>
2024-08-01 10:53:57 +0300

    Write block to file with in random order using offset

--- torrent/file/write.go
+++ torrent/file/write.go
@@ -2,6 +2,8 @@ package file
 
 import (
 	"os"
+	"io"
+	"fmt"
 	"byo_bittorrent/torrent/metadata/file"
 	"byo_bittorrent/torrent/p2p"
 )
@@ -15,6 +17,12 @@ func (w *Writer) fileName() string {
 }
 
 func (w *Writer) saveBlock(file *os.File, block *p2p.Block) error {
+	offset := int64(block.Index * w.Torrent.PieceLength)
+
+	if _, err := file.WriteAt(block.Buffer, offset); err != nil {
+		return fmt.Errorf("Failed to write block: ", err)
+	}
+
 	return nil
 }
 
@@ -27,11 +35,13 @@ func (w *Writer) Write(blocks chan p2p.Block) error {
     if err != nil {
         panic(err)
     }
+	defer file.Close()
 
 	for len(blocks) > 0 {
 		block := <- blocks
 
 		if err := w.saveBlock(file, &block); err != nil {
+			// if failed to write block to file, return block to the channel
 			blocks <- block
 		}
 	}
