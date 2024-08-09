2b69e8e385a33872ad28877c8ff784216064a9a0
PearlFoe <itknml@gmail.com>
2024-08-09 19:40:11 +0300

    Init progress bar logic

---
+++ cli/progress.go
@@ -0,0 +1,29 @@
+package cli
+
+import (
+	"byo_bittorrent/torrent/p2p"
+	"time"
+
+	pb "github.com/schollz/progressbar/v3"
+)
+
+type ProgressBar struct {
+	Bitfield *p2p.Bitfield
+}
+
+func (b *ProgressBar) Start() {
+	bfLength := b.Bitfield.Lenght()
+	bar := pb.Default(int64(bfLength))
+
+	downloaded := 0
+	for downloaded < bfLength {
+		newDownloaded := b.Bitfield.CountDownloaded()
+
+		if newDownloaded > downloaded {
+			bar.Add(newDownloaded - downloaded)
+			downloaded = newDownloaded
+		}
+
+		time.Sleep(40 * time.Millisecond)
+	}
+}
